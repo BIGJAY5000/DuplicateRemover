decfae98bcc115c0dd3f95bc67ad351a511c2061
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-11-09 09:20:43 -0500

    rgbfdmgmm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        String s;
+        String current;
+        String newWord = "";
+        System.out.println("Please enter a string");
+        s = scan.nextLine();
+        for (int i = 0; i < s.length(); i++) {
+            current = s.substring(i, i + 1);
+            if (i == 0 || !s.substring(0, i).contains(current)) {
+                newWord = newWord + current;
+            }
+        }
+        System.out.println("Your new word is " + newWord);
     }
 }
